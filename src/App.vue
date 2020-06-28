dc64838eb7b7607443b413dcc4c0fd23ecc8e133
Dmitry Moiseenko <dmitrymoiseenko@outlook.com>
2020-06-28 09:42:56 +0300

    Add mixins

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,40 @@ export default {
 </script>
 
 <style lang="scss">
-@import './assets/fonts/fonts.scss';
+@import './assets/scss/fonts.scss';
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  height: 100%;
+  // Set 1rem equal to 10px, where default font-size is 16px
+  // 100% = 16xp and X% = 10px
+  // 100 * 10 / 16 = 62.5
+  font-size: 62.5%;
+}
+
+body {
+  height: 100%;
+  font-size: 1.6rem;
+  font-family: "TT Travels";
+}
+
+ul {
+  list-style-type: none;
+}
+
+button {
+  cursor: pointer;
+  outline: none;
+}
+
+#app {
+  min-width: 32rem;
+  height: 100%;
+  position: relative;
+}
 </style>
